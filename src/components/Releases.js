5f60e72670e83f6f691d8dfd386a68f9449b3c3d
Christian Lessa <christianclessa@gmail.com>
2023-03-09 20:44:22 -0300

    implementado lógica de mudar imagem ao passar o mouse

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -4,6 +4,28 @@ import { allImages } from '../data/imagesMock';
 function Releases() {
   const [imageSrc, setImageSrc] = useState(allImages);
 
+  function handleMouseOver(imageId, imageOver) {
+    const updatedImage = imageSrc.map((image) => {
+      if (image.id === imageId) {
+        return { ...image, img: imageOver }
+      }
+      return image;
+    });
+    setImageSrc(updatedImage);
+    console.log("entrou id é: ", imageId, "estado: ", imageSrc)
+  }
+
+  function handleMouseOut(imageId, imageOut) {
+    const updatedImage = imageSrc.map((image) => {
+      if (image.id === imageId) {
+        return { ...image, img: imageOut }
+      }
+      return image;
+    });
+    setImageSrc(updatedImage);
+    console.log("saiu id é: ", imageId, "estado: ", imageSrc)
+  }
+
   return (
     <>
       <h1 className="release">Lançamentos</h1>
@@ -16,6 +38,8 @@ function Releases() {
                   src={image.img}
                   alt={image.title}
                   className="release-image"
+                  onMouseOver={ () => handleMouseOver(image.id, image.imgOver) }
+                  onMouseOut={ () => handleMouseOut(image.id, image.imgOut) }
                 />
                 <h3>{image.title}</h3>
                 <button
